cd800ce6bcb1f5fbdb77ea17bd94272b5a3f605e
rushiM2201 <rushikeshmitech@gmail.com>
2024-05-24 15:01:36 +0530

    contactus page

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'skill_forge',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
